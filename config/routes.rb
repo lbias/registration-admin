38c80c27c6a9982c082a411adb6266241e99045d
lbias <306508354@qq.com>
2017-05-27 00:51:19 +0800

    routes for admin::event/tickets

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,12 @@ Rails.application.routes.draw do
   resources :events
 
   namespace :admin do
-    resources :events
+    resources :events do
+      resources :tickets, :controller => "event_tickets"
+    end
     resources :users do
       resource :profile, :controller => "user_profiles"
-    end 
+    end
   end
 
   root "events#index"
